0c61199a23285f592eda44f9f9c66a54d05f40c7
Dmitriy Kuminov <coding@dmik.org>
2017-06-08 14:09:10 +0400

    Make converter scripts search for helper script in their own directory.
    This also adds handling a failure to find the helper script.

--- exe/rexx2pm.cmd
+++ exe/rexx2pm.cmd
@@ -1,3 +1,9 @@
 /* REXX2PM: bind resource to REXX_PM.EXE */
+Parse Source . . Source
+helper = FileSpec('d', Source) || FileSpec('p', Source) || 'rexx2xx.cmd'
+if Stream(helper, 'C', 'Query Exist') = '' then do
+  Say 'FATAL: Helper "'helper'" is not found.'
+  exit 1
+end
 Parse Arg Args
-return rexx2xx.cmd('@PM' Args)
+'@call' helper '@PM' Args
--- exe/rexx2vio.cmd
+++ exe/rexx2vio.cmd
@@ -1,3 +1,9 @@
 /* REXX2VIO: bind resource to REXX_VIO.EXE */
+Parse Source . . Source
+helper = FileSpec('d', Source) || FileSpec('p', Source) || 'rexx2xx.cmd'
+if Stream(helper, 'C', 'Query Exist') = '' then do
+  Say 'FATAL: Helper "'helper'" is not found.'
+  exit 1
+end
 Parse Arg Args
-return rexx2xx.cmd('@VIO' Args)
+'@call' helper '@VIO' Args
